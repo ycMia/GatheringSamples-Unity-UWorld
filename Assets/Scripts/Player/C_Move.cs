c05b5b09981523c8ab3c3403390e76b75b956567
a122a057 <lianyue1014774856@163.com>
2022-08-18 11:04:10 +0800

    a

--- Assets/Scripts/Player/C_Move.cs
+++ Assets/Scripts/Player/C_Move.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using Interface;
+using MyScripts.Logics.Time;
 
+//以下为可移动的物体的类
 namespace Class
 {
-    //以下为可移动的物体的类
     public class C_Move : MonoBehaviour, I_K_Move, I_M_Move
     {
+        void Update()
+        {
+            if(Input.GetButtonDown("Jump"))
+            f = true;
+        }
         void FixedUpdate()
         {
             K_Movement();
@@ -16,6 +21,7 @@ namespace Class
         public Rigidbody2D rb;
         public float speed;
         public float jumpforce;
+        private bool f = false;
         public void K_Movement()
         {
             float horizontalmove;
@@ -26,14 +32,19 @@ namespace Class
             {
                 rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
             }
-            if (Input.GetButtonDown("Jump"))
+            if (f)
             {
+                TimerHub.Instance.AddClockRent("a");
                 rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+                TimerHub.Instance.SweepOutClock("a");
+                f = false;
+                TimerHub.Instance.SweepOutClock("a");
             }
         }
         public void M_Movement()
         {
             ;
         }
+
     }
 }
