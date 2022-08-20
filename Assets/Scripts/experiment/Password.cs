1b1b884beee6e1cf94ad3456596efea220cc7a16
a122a057 <lianyue1014774856@163.com>
2022-08-20 10:33:09 +0800

    a

---
+++ Assets/Scripts/experiment/Password.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MyScripts.Logics.Message;
+using MyScripts.Interactable;
+using MyScripts.CursorControl;
+using MyScripts.CursorControl.State;
+using MyScripts.Logics.StateMachine;
+using System;
+
+namespace Password.a
+{
+    public class Password : SimpleMessageSender<PasswordResult>
+    {
+        private string CPassword = "wycnb";
+        public Text te;
+        public void Click()
+        {
+            SendMessageToCortex(new PasswordResult(te.text, CPassword == te.text));
+        }
+
+    }
+    public class PasswordResult{
+        public PasswordResult(string key,bool flag)
+        {
+            this.key = key;
+            this.flag = flag;
+        }
+        public string key;
+        public bool flag;
+    }
+}
