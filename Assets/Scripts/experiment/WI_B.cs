ad1ed8e50a1f54d7c1848163b2c951b2e8926105
a122a057 <lianyue1014774856@163.com>
2022-08-20 09:16:43 +0800

    a

---
+++ Assets/Scripts/experiment/WI_B.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyScripts.Logics.Message;
+
+namespace W_B
+{
+    public class WI_B : SimpleMessageSender<WIFI>
+    {
+        public WIFI me;
+        public GameObject TP;
+        public GameObject re;
+        public void Cli()
+        {
+            if (re.activeInHierarchy)
+            {
+                me = new WIFI(TP, true);
+                SendMessageToCortex(me);
+            }
+            else
+            {
+                me = new WIFI(TP, false);
+                SendMessageToCortex(me);
+            }
+        }
+    }
+    public class WIFI
+    {
+        public GameObject gameobject;
+        public bool Can;
+        public WIFI(GameObject x,bool y)
+        {
+            gameobject = x;
+            Can = y;
+        }
+    }
+}
